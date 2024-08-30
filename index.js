ce58c4a3bfba7c0835f99cd84503335d52f15d0d
nurulislamrimon <nurulislamrimon@gmail.com>
2024-08-30 11:53:48 +0600

    query done

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
-const dgram = require('node:dgram');
-const server = dgram.createSocket('udp4');
+const dgram = require("dgram");
+const dnsPacket = require("dns-packet");
+const server = dgram.createSocket("udp4");
 
-server.on('error', (err) => {
+server.on("error", (err) => {
   console.error(`server error:\n${err.stack}`);
   server.close();
 });
 
-server.on('message', (msg, rinfo) => {
-  console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+// server.on("message", (msg, rinfo) => {
+//   console.log(`server got: ${msg.toString()} from ${{rinfo}}.`);
+// });
+
+const myDb = {
+  "expertsquad.net": "198.175.150.9",
+  "theqprint.com": "62.171.145.157",
+};
+
+server.on("message", (message, rInfo) => {
+  const incomingReq = dnsPacket.decode(message);
+  const questions = incomingReq.questions;
+  const resolvedIP = myDb[questions?.[0]?.name];
+
+  const ans = dnsPacket.encode({
+    type: "response",
+    id: incomingReq.id,
+    flags: dnsPacket.AUTHORITATIVE_ANSWER,
+    questions: questions,
+    answers: [
+      {
+        type: "A",
+        class: "IN",
+        name: questions?.[0]?.name,
+        data: resolvedIP,
+      },
+    ],
+  });
+
+  server.send(ans, rInfo.port, rInfo.address);
+
+  console.log(questions, rInfo, resolvedIP); // prints out a response from google dns
 });
 
-server.on('listening', () => {
+server.on("listening", () => {
   const address = server.address();
   console.log(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind(53,()=>{
-    console.log("DNS server is running on port : 53");
-    
+server.bind(5353, () => {
+  console.log("DNS server is running on port : 5353");
 });
